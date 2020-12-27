f0d81f1e7163bdd9d8184ac2b83cae7f0b1ec514
Nikolai Weidt <weidtn@gmail.com>
2020-12-27 23:29:53 +0100

    gave points also a default value of 5000

--- src/weidtn/fun_gen.clj
+++ src/weidtn/fun_gen.clj
@@ -12,7 +12,7 @@
 
 (defn sin
   "Returns the sine wave with amplitude, frequency and phase shifted with given datapoints"
-  ([points & {:keys [ampl freq phase] :or {ampl 1 freq 1 phase 0}}]
+  ([& {:keys [points ampl freq phase] :or {points 1000 ampl 1 freq 1 phase 0}}]
    (let [phase-rad (deg-to-rad phase)]
      (for [x (range 0 (* 2 Math/PI) (/ (* 2 Math/PI) points))] ;; this line works!
        {:x x :y (* ampl (Math/sin (* freq (+ x phase-rad))))}))))
@@ -21,17 +21,10 @@
 (oz/start-server!)
 
 (def line-plot
-  {:data {:values (sin 5000 :ampl 3 :freq 3 :phase 30)}
+  {:data {:values (sin)}
    :encoding {:x {:field "x" :type "quantitative"}
               :y {:field "y" :type "quantitative"}}
    :mark "line"})
 
 ;; Render the plot
 (oz/view! line-plot)
-
-(defn play-data [& names]
-  (for [n names
-        i (range 20)]
-    {:time i :item n :quantity (+ (Math/pow (* i (count n)) 0.8) (rand-int (count n)))}))
-
-(take 1 (play-data "hello"))
