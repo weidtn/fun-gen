3674da4cfd9aa3c82f20ef0ac06cab28a0ba0f21
Nikolai Weidt <weidtn@gmail.com>
2020-12-27 23:23:27 +0100

    working sine function

--- src/weidtn/fun_gen.clj
+++ src/weidtn/fun_gen.clj
@@ -1,7 +1,6 @@
 (ns weidtn.fun-gen
   (:gen-class)
-  (:require [oz.core :as oz]
-            [infix.macros :refer [$=]]))
+  (:require [oz.core :as oz]))
 
 (defn deg-to-rad
   [deg]
@@ -11,47 +10,28 @@
   [rad]
   (* rad (/ 180 Math/PI)))
 
-;; Create x-data
-;;
-(defn create-x
-  "Creates x-data between zero and 2PI with a certain number of points to create functions with"
-  [points]
-  (range 0 (* 2 Math/PI) (/ (* 2 Math/PI) points)))
-
-(defn sin 
-  "Returns the sine wave with amplitude, frequency and phase shifted for given x"
-  ([x & {:keys [ampl freq phase] :or {ampl 1 freq 1 phase 0}}]
+(defn sin
+  "Returns the sine wave with amplitude, frequency and phase shifted with given datapoints"
+  ([points & {:keys [ampl freq phase] :or {ampl 1 freq 1 phase 0}}]
    (let [phase-rad (deg-to-rad phase)]
-     (->> x
-          (map #(+ phase-rad %) ,,,)
-          (map #(* freq %) ,,,)
-          (map #(Math/sin %) ,,,)
-          (map #(* ampl %) ,,,)))))
-
-(defn create-map
-  "TODO: Creates a map from x and y {:x x :sin (sin x)}"
-  [xs ys]
-  (for [x xs
-        y ys]
-    (,,,,,,,)))
-
-;; Testing
-(def x (create-x 1000)) ; 1000 datapoints between 0 and 2pi
-(def default-sin (sin x))
-
-(create-map x default-sin)
-
-(take 3 (sin x)) ; default works
-;; => (0.0 0.006283143965558951 0.012566039883352607)
-
-(apply max (sin x :ampl 4 :freq 1)) ; amplitude works
-;; => 4.0
-
-(take 3 (sin x :freq 10)) ; frequency works
-;; => (0.0 0.06279051952931337 0.12533323356430426)
-
-(take 3 (sin x :phase 30)) ; phase shifting works
-;; => (0.49999999999999994 0.5054314927178775 0.5108430318658598)
+     (for [x (range 0 (* 2 Math/PI) (/ (* 2 Math/PI) points))] ;; this line works!
+       {:x x :y (* ampl (Math/sin (* freq (+ x phase-rad))))}))))
 
 ;; plotting 
-;; (oz/start-server!)
+(oz/start-server!)
+
+(def line-plot
+  {:data {:values (sin 5000 :ampl 3 :freq 3 :phase 30)}
+   :encoding {:x {:field "x" :type "quantitative"}
+              :y {:field "y" :type "quantitative"}}
+   :mark "line"})
+
+;; Render the plot
+(oz/view! line-plot)
+
+(defn play-data [& names]
+  (for [n names
+        i (range 20)]
+    {:time i :item n :quantity (+ (Math/pow (* i (count n)) 0.8) (rand-int (count n)))}))
+
+(take 1 (play-data "hello"))
